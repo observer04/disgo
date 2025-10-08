06523ab1f00c52aa6df5a8acaa2709d235913889
observer04 <tripathysarojinee@gmail.com>
2025-10-09 00:03:07 +0530

    write pong to accepted connection

--- app/main.go
+++ app/main.go
@@ -2,39 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
 var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Use the standard logger for messages and errors
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	log.Fatalf("Failed to bind to port 6379: %v", err)
-	// }
-	// log.Printf("Listening on 0.0.0.0:6379")
-
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	log.Fatalf("Error accepting connection: %v", err)
-	// }
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
 	if err != nil {
-		fmt.Println("Failed to bind to port ")
-		os.Exit(1)
+		log.Fatal("Failed to bind to port 6379", err)
 	}
 
-	_, err = l.Accept()
+	con, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		log.Fatal("Error accepting connection: ", err.Error())
 	}
+	con.Write([]byte("+PONG\r\n"))
 }
