1a9c8df50e7ce96cd38de4a3d505d475fb6f2cad
observer04 <tripathysarojinee@gmail.com>
2025-10-08 21:57:25 +0530

    [any message]

--- app/main.go
+++ app/main.go
@@ -14,16 +14,27 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
+	// Use the standard logger for messages and errors
 	// l, err := net.Listen("tcp", "0.0.0.0:6379")
 	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
+	// 	log.Fatalf("Failed to bind to port 6379: %v", err)
 	// }
+	// log.Printf("Listening on 0.0.0.0:6379")
+
 	// _, err = l.Accept()
 	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
+	// 	log.Fatalf("Error accepting connection: %v", err)
 	// }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
+	if err != nil {
+		fmt.Println("Failed to bind to port ")
+		os.Exit(1)
+	}
+
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
